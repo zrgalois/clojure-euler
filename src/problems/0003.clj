76ae998f7ebb4e2603df86d997b7cb465d3d6a7c
Zachary Rivett <zacharyrivett@gmail.com>
2020-02-09 14:18:15 -0600

    Fixed erasothenes
    Wasn't using the calculated upper bound of primes to check.
    This pushes it back to the top.
    Which, although slightly disappointing, makes perfect sense.

--- src/problems/0003.clj
+++ src/problems/0003.clj
@@ -8,12 +8,12 @@
             (while (zero? (mod @n-atom p))
               (swap! n-atom quot p))
             (and (<= @n-atom p) p))
-          (primes/primes-zach))))
+          (primes/primes-erasothenes))))
 
 #_(time (s-0003 600851475143))
 #_(time (s-0003 (*' 937 937 937 937 937 600851475143)))
 #_(time (s-0003 600851475143123))
-;; This takes a while... (106.62 minutes, or ~1.78 hrs)
+;; This takes a while...
 ;; Apparently, I accidentally picked a number with exactly 2 prime factors.
 ;; 4540079 and 132343837
 ;; #AccidentalRSA
--- src/utils/primes.clj
+++ src/utils/primes.clj
@@ -1,23 +1,29 @@
 (ns utils.primes
   (:require [clojure.math.numeric-tower :as math]))
+;; Fastest approach is pulling in a large list of precalculated primes,
+;;  rather than recalculating them each time.
+;; But that's no fun.
 
-;; Decided to do my own algorithm.
-;; Fun to write; interested in relative performance.
-;; Basic premise is as we build up the list of primes,
-;;  keep a tab on the next multiple of each prime.
-;; If the number equals one of those, we can go ahead and
-;;  scratch it off the list.
-;; If it is greater than a multiple, we advance that multiple to the next.
-;; If it is less than a multiple, we know that it is prime, because
-;;  our multiples are sorted from least to greatest.
-;; (Keeping it in a sorted set is an attempt to increase performance;
-;;  it gives us the ability to do early exits.)
-;; [If I really wanted to increase performance, I'd probably use a mutable
-;;  sorted collection directly, rather than wrapping a persistent tree set
-;;  in an atom.]
-;; You'll also see a few small optimizations, such as taking 2 as a base case,
-;; not checking even numbers, and avoiding even numbers in our composites list.
-(defn primes-zach
+;; Erasothenes
+(defn trial-division-succeeds? [n primes]
+  (let [ub (math/expt n 1/2)]
+    (->> primes (take-while (partial >= ub)) (some #(zero? (mod n %))))))
+
+;; To be fair, will have both algorithms skip even numnbers:
+(defn primes-erasothenes
+  ([] (cons 2 (primes-erasothenes 3 [2])))
+  ([n primes]
+   (if (trial-division-succeeds? n primes)
+     (primes-erasothenes (+ n 2) primes)
+     (lazy-seq (cons n (primes-erasothenes (+ n 2) (conj primes n)))))))
+
+;; Custom algorithm;
+;;  along with the growing list of primes, it keeps a sorted set
+;;  of composites to strike off candidates without resorting to division.
+;; Fun to write, but its more complicated, and doesn't run as fast.
+;; There are probably some opportunities to increase performance further.
+;; (Better underlying data structure; avoiding overlap in composites.)
+(defn- primes-zach
   ([] (cons 2 (primes-zach 3 (atom (sorted-set [9 3])))))
   ([n composites]
    (if (= :prime
@@ -46,19 +52,6 @@
      (lazy-seq (cons n (primes-zach (+ n 2) composites)))
      (primes-zach (+ n 2) composites))))
 
-;; Now for Erasothenes:
-(defn trial-division-succeeds? [n primes]
-  (let [ub (math/expt n 1/2)]
-    (->> primes (some #(zero? (mod n %))))))
-
-;; To be fair, will have both algorithms skip even numnbers:
-(defn- primes-erasothenes
-  ([] (cons 2 (primes-erasothenes 3 [2])))
-  ([n primes]
-   (if (trial-division-succeeds? n primes)
-     (primes-erasothenes (+ n 2) primes)
-     (lazy-seq (cons n (primes-erasothenes (+ n 2) (conj primes n)))))))
-
 ;; Euler algorithm:
 ;; (probably poorly implemented; this blows its stack easily.)
 ;; (there might be a way to make this into a lazy sequence like
@@ -82,35 +75,19 @@
 ;;;;; ;;; ;; ; ;  ;   ;     ;
 ;; Superficial benchmarking ;
 ;;;;; ;;; ;; ; ;  ;   ;     ;
-
-;; Euler's is fastest for small numbers:
 #_(let [n 1753
         _ (print "eras:") p1s (time (doall (take n (primes-erasothenes))))
         _ (print "zach:") p2s (time (doall (take n (primes-zach))))
         last-prime 14983
         _ (print "eule:") p3s (time (doall (primes-euler (inc last-prime))))]
     (time (every? true? (map = p1s p2s p3s))))
-;; But, it quickly reaches stack overflows for larger ones:
-#_(primes-euler 15000)
-#_(primes-euler 100000)
-;; (Remember that this isn't asking for the 100000th prime;
-;;  it is asking that the first 100000 numbers be filtered.)
 
-;; My algorithm prevails with larger ones:
 #_(let [n 10000
       _ (print "eras:") p1s (time (doall (take n (primes-erasothenes))))
       _ (print "zach:") p2s (time (doall (take n (primes-zach))))]
   (time (every? true? (map = p1s p2s))))
 
-;; (This one takes some time;
-;;  Erasothenes ~ 11.66 minutes
-;;  Mine        ~ 00.24 minutes)
 #_(let [n 100000 ; One hundred thousand
       _ (print "eras:") p1s (time (doall (take n (primes-erasothenes))))
       _ (print "zach:") p2s (time (doall (take n (primes-zach))))]
   (time (every? true? (map = p1s p2s))))
-
-;; The million dollar question:
-#_(time (doall (take 1000000 (primes-zach))))
-;; Takes ~ 3.76 minutes to run
-;; 15,485,863 is the 1,000,000th prime.
